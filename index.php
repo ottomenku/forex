2466730e810358e8bbc07acdf594e47e058faaf3
Ménkű Ottó <ottomenku@gmail.com>
2022-10-07 17:30:23 +0200

    one

--- index.php
+++ index.php
@@ -1,1 +1,25 @@
-forex index
+<?<php>
+$tick=[];
+$tick["bid"]=$_GET['bid'];
+$tick["ask"]=$_GET['ask'];
+$tick["vol"]=$_GET['vol'];
+$tick["time"]=$_GET['time'];
+$ret='none';
+$history=[];
+$history[]=$tick;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo $ret;
+</php>
